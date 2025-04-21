86d63a23d842d32281fc29245305319067e9a57d
petchluvsyou <petchluvsyou@outlook.com>
2025-04-22 00:07:30 +0700

    feat: group chat message

--- client/src/pages/groupchat.tsx
+++ client/src/pages/groupchat.tsx
@@ -5,12 +5,14 @@ import { User } from "@/types/user";
 import * as React from "react";
 import { useParams } from "react-router";
 import { Info } from "lucide-react";
+import { MessageInput } from "@/components/chat/message-input";
 
 export function GroupChat() {
   const { groupid } = useParams();
   const othername = "admin999";
   const [other, setOther] = React.useState<User | null>(null);
   const [loading, setLoading] = React.useState(true);
+  const [currentMessage, setCurrentMessage] = React.useState<string>("");
 
   React.useEffect(() => {
     async function fetchOther() {
@@ -43,24 +45,29 @@ export function GroupChat() {
   }
 
   return (
-    <div className="flex w-full flex-col">
-      <header className="flex w-full items-center justify-between border-b p-4">
-        <div className="flex items-center gap-4">
-          <Avatar>
-            <AvatarImage src={other?.image} alt={othername} />
-            <AvatarFallback>{othername?.slice(0, 2)}</AvatarFallback>
-          </Avatar>
-          <h2 className="text-lg font-semibold">{othername}</h2>
+    <div className="flex size-full flex-col justify-between">
+      <div className="flex w-full flex-col">
+        <header className="flex w-full items-center justify-between border-b p-4">
+          <div className="flex items-center gap-4">
+            <Avatar>
+              <AvatarImage src={other?.image} alt={othername} />
+              <AvatarFallback>{othername?.slice(0, 2)}</AvatarFallback>
+            </Avatar>
+            <h2 className="text-lg font-semibold">{othername}</h2>
+          </div>
+          <Info />
+        </header>
+        <div className="flex w-full flex-col gap-2 p-4">
+          <Bubble createdAt={new Date()} sender={other} variant="received">
+            Hi
+          </Bubble>
+          <Bubble createdAt={new Date()} variant="sent">
+            Hi back
+          </Bubble>
         </div>
-        <Info />
-      </header>
-      <div className="flex w-full flex-col gap-2 p-4">
-        <Bubble createdAt={new Date()} sender={other} variant="received">
-          Hi
-        </Bubble>
-        <Bubble createdAt={new Date()} variant="sent">
-          Hi back
-        </Bubble>
+      </div>
+      <div className="mb-4 flex w-full">
+        <MessageInput value={currentMessage} onChange={setCurrentMessage} />
       </div>
     </div>
   );
